30389285dcfc5597dd6eefdc5e8900e4d218ad1d
vgrunev <vgrunev@VGRUNEV>
2016-12-21 18:04:27 +0700

    commented all vulnerabilities

--- Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
+++ Tests/1 INPUT DATA VERIFICATION/9 LDAP Injection/Ldap.aspx.cs
@@ -14,52 +14,52 @@ namespace Mopas.Tests
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-			Console.Write("Tests");
-            // TODO this todo was here for ages
-            var dc = new DirectoryContext(DirectoryContextType.Domain, "ptsecurity.ru");
+			//Console.Write("Tests");
+   //         // TODO this todo was here for ages
+   //         var dc = new DirectoryContext(DirectoryContextType.Domain, "ptsecurity.ru");
 
-            var address = Request.Params["address"];
-            var filter = "Address=" + address;
-            var result = "";
+   //         var address = Request.Params["address"];
+   //         var filter = "Address=" + address;
+   //         var result = "";
 
-            var domain = Domain.GetDomain(dc);
+   //         var domain = Domain.GetDomain(dc);
 
-            // this is our vulnerabilitiy of LDAP injection *in this file*
-            var ds = new DirectorySearcher(domain.GetDirectoryEntry(), filter);
+   //         // this is our vulnerabilitiy of LDAP injection *in this file*
+   //         var ds = new DirectorySearcher(domain.GetDirectoryEntry(), filter);
 
-			using (var src = ds.FindAll())
-			{
-				// TODO it was edit here by developer 1 year ago
-				foreach (var res in src)
-				{
-					result = res.ToString();
-				}
-			}
+			//using (var src = ds.FindAll())
+			//{
+			//	// TODO it was edit here by developer 1 year ago
+			//	foreach (var res in src)
+			//	{
+			//		result = res.ToString();
+			//	}
+			//}
 
-			using (var src = ds.FindAll())
-			{
-				// TODO it was edit here by developer 1 year ago
-				foreach (var res in src)
-				{
-					result = res.ToString();
-				}
-			}
+			//using (var src = ds.FindAll())
+			//{
+			//	// TODO it was edit here by developer 1 year ago
+			//	foreach (var res in src)
+			//	{
+			//		result = res.ToString();
+			//	}
+			//}
 
-			// this is our first vulnerability of XSS in this file
-			// we will demonstrate False Positive scenario here (FP Marker)
-			Response.Write(result);
+			//// this is our first vulnerability of XSS in this file
+			//// we will demonstrate False Positive scenario here (FP Marker)
+			//Response.Write(result);
 
-            // this is our second vulnerability of XSS in this file
-            // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
-            Response.Write(result);
+   //         // this is our second vulnerability of XSS in this file
+   //         // we will demonstrate what happen if developer fails with his fix (VERIFY Marker)
+   //         Response.Write(result);
 
-            // this is our third vulnerability of XSS in this file
-            // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
-            Response.Write(result);
+   //         // this is our third vulnerability of XSS in this file
+   //         // we will demonstrate what happen if we really fix vulnerability (VERIFY Marker)
+   //         Response.Write(result);
 
-            // this is our fourth vulnerability of XSS in this file
-            // we will demonstrate what happen if developer want to cheat (FIXED Marker)
-            Response.Write(result);
+   //         // this is our fourth vulnerability of XSS in this file
+   //         // we will demonstrate what happen if developer want to cheat (FIXED Marker)
+   //         Response.Write(result);
         }
     }
 }
